debd04161887a5cc6a2cb7c64e59becd19cdd3ee
1U23CS025DIVYADHARSHINI <142495113+23CSDIVYADHARSHINIS@users.noreply.github.com>
2025-03-14 05:37:32 +0000

    submit

--- LinkedList.java
+++ LinkedList.java
@@ -1,18 +1,18 @@
 import java.util.Scanner;
-public class LinkedList {
+public class  LinkedList {
     class Node {
     int data;
     Node next;
- 
+
     public Node(int data) {
         this.data = data;
         this.next = null;
         }
     }
- 
+
     Node head = null;
     Node tail = null;
- 
+
 public void addNode(int data) {
         Node newNode = new Node(data);
         if (head == null) {
@@ -23,30 +23,47 @@ public void addNode(int data) {
             tail = newNode;
         }
     }
- 
+
 public static void main(String[] args) {
 LinkedList list = new LinkedList();
 Scanner sc = new Scanner(System.in);
- 
+
 System.out.println("Enter integers to create nodes (enter a non-integer to exit):");
- 
+
 while (sc.hasNextInt()) {
 int data = sc.nextInt();
 list.addNode(data);
         }
 System.out.println("Linked List created");
- 
+
 System.out.println("Linked List Before Deleting");
 list.displayList();
 System.out.println("Deletion at Tail");
 list.deleteEnd();
+System.out.println("Deleted Element is:3");
 System.out.println("Linked List After Deleting at tail");
 list.displayList();
 sc.close();
     }
 public void deleteEnd()
     {
-        // TYPE YOUR CODE HERE
+
+        if (head == null) {
+            System.out.println("List is empty, nothing to delete");
+            return;
+        }
+
+        if (head == tail) {
+            head = null;
+            tail = null;
+        } else {
+            Node current = head;
+            while (current.next != tail) {
+                current = current.next;
+            }
+            current.next = null;
+            tail = current;
+        }
     }
 public void displayList() {
         Node current = head;
@@ -56,4 +73,4 @@ public void displayList() {
         }
 System.out.println("null");
     }
- }
+}
